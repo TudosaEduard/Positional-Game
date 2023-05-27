2108030e83ad9a038a8f485eed8ccf415a25be60
Tudosa Eduard <tudosa.bogdan88@gmail.com>
2023-05-27 21:38:22 +0300

    login and register page with java swing

--- UI/src/main/java/gui/login/Login.java
+++ UI/src/main/java/gui/login/Login.java
@@ -1,4 +1,30 @@
 package gui.login;
 
-public class Login {
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JDialog{
+    private JPanel loginPanel;
+    private JPanel welcomePanel;
+    private JLabel welcomeText1;
+    private JTextField usernameLoginField;
+    private JPasswordField passwordLoginField;
+    private JButton loginButton;
+    private JLabel welcomeText2;
+    private JPanel loginNowPanel;
+    private JLabel titleLogin;
+    private JLabel usernameLogin;
+    private JLabel passwordLogin;
+
+    public Login(JFrame frame) {
+        super(frame, "Access your account", true);
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(500, 500));
+        setLocationRelativeTo(frame);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login(null);
+    }
 }
--- UI/src/main/java/gui/register/Register.java
+++ UI/src/main/java/gui/register/Register.java
@@ -1,4 +1,31 @@
 package gui.register;
 
-public class Register {
+import javax.swing.*;
+import java.awt.*;
+
+public class Register extends JDialog{
+    private JPanel registerPanel;
+    private JTextField usernameRegisterField;
+    private JTextField passwordRegisterField1;
+    private JPasswordField passwordRegisterField2;
+    private JTextField emailRegisterField;
+    private JButton registerButton;
+    private JLabel titleRegister;
+    private JLabel usernameRegister;
+    private JLabel passwordRegister1;
+    private JLabel passwordRegister2;
+    private JLabel emailRegister;
+    private JLabel loginNowButton;
+
+    public Register(JFrame frame) {
+        super(frame, "Create a new account", true);
+        setContentPane(registerPanel);
+        setMinimumSize(new Dimension(500, 500));
+        setLocationRelativeTo(frame);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Register register = new Register(null);
+    }
 }
